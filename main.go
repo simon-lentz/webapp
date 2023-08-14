3ff22079b0c649a9977b23889bf46ff9655325ba
simon-lentz <simonlentz1@gmail.com>
2023-08-14 15:12:03 -0400

    initial commit with most basic arch

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// HTTP handler functions.
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Test Site Home</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<h1>Contact</h1>
+	<p>e: <a href=\"mailto:simonlentz1@gmail.com\">simonlentz1@gmail.com</a>.`)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>About</h1>")
+}
+
+// Link handlers to their respective URLs.
+type Router struct{}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/about":
+		aboutHandler(w, r)
+	default:
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	}
+}
+
+// Make new router with Chi, register handler functions,
+// listen and serve on port 3000.
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/about", aboutHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	})
+	fmt.Println("Starting server on :3000...")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		// TODO handle with log/slog
+		fmt.Println("Fatal Error")
+	}
+}
