566b241b2fe548ac81eb0664a17854a45176103a
simon-lentz <simonlentz1@gmail.com>
2023-08-17 16:49:27 -0400

    Finished setup for SMTP for forgot password email.

--- models/email.go
+++ models/email.go
@@ -1,13 +1,21 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+
+	"github.com/go-mail/mail/v2"
+)
 
 const (
 	DefaultSender = "support@webapp.com"
 )
 
 type Email struct {
-	//
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
 }
 
 type SMTPConfig struct {
@@ -15,6 +23,13 @@ type SMTPConfig struct {
 	Port     int
 	Username string
 	Password string
+}
+
+func NewEmailService(config SMTPConfig) (*EmailService, error) {
+	es := EmailService{
+		dialer: mail.NewDialer(config.Host, config.Port, config.Username, config.Password),
+	}
+	return &es, nil
 }
 
 type EmailService struct {
@@ -27,9 +42,51 @@ type EmailService struct {
 	dialer *mail.Dialer
 }
 
-func NewEmailService(config SMTPConfig) (*EmailService, error) {
-	es := EmailService{
-		dialer: mail.NewDialer(config.Host, config.Port, config.Username, config.Password),
+func (es *EmailService) Send(email Email) error {
+	msg := mail.NewMessage()
+	msg.SetHeader("To", email.To)
+	es.setFrom(msg, email)
+	msg.SetHeader("Subject", email.Subject)
+	switch {
+	case email.Plaintext != "" && email.HTML != "":
+		msg.SetBody("text/plain", email.Plaintext)
+		msg.AddAlternative("text/html", email.HTML)
+	case email.Plaintext != "":
+		msg.SetBody("text/plain", email.Plaintext)
+	case email.HTML != "":
+		msg.SetBody("text/html", email.HTML)
 	}
-	return &es, nil
+
+	if err := es.dialer.DialAndSend(msg); err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+
+	return nil
+}
+
+func (es *EmailService) ForgotPassword(to, resetURL string) error {
+	email := Email{
+		Subject:   "Reset Password",
+		To:        to,
+		Plaintext: "To reset your password, please visit the following link: " + resetURL,
+		HTML:      `<p>To reset your password, please visit the following link: <a href="` + resetURL + `">` + resetURL + `</a></p>`,
+	}
+
+	if err := es.Send(email); err != nil {
+		return fmt.Errorf("forgot password email: %w", err)
+	}
+	return nil
+}
+
+func (es *EmailService) setFrom(msg *mail.Message, email Email) {
+	var from string
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+	msg.SetHeader("From", from)
 }
