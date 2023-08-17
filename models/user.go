ecf071df34c180d03ffa24b9fef6809ce7ab1f1a
simon-lentz <simonlentz1@gmail.com>
2023-08-17 07:53:45 -0400

    added query by email and compare hash and password to the UserService Authenticate method.

--- models/user.go
+++ models/user.go
@@ -61,7 +61,29 @@ func (us *UserService) Create(email, password string) (*User, error) {
 		VALUES ($1, $2) RETURNING id;`, email, passwordHash)
 
 	if err = row.Scan(&user.ID); err != nil {
-		return nil, fmt.Errorf("create user: %v", err)
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (us *UserService) Authenticate(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	user := User{
+		Email: email,
+	}
+
+	row := us.DB.QueryRow(`
+	SELECT id, password_hash
+	FROM users WHERE email=$1`, email)
+
+	if err := row.Scan(&user.ID, &user.PasswordHash); err != nil {
+		return nil, fmt.Errorf("authenticate: %w", err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password)); err != nil {
+		fmt.Printf("Invalid Password: %v\n", password)
+		return nil, fmt.Errorf("authenticate: %w", err)
 	}
 
 	return &user, nil
