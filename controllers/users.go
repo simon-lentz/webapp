654cc7a2025823b01e3847abbf83f26b079bc4cc
simon-lentz <simonlentz1@gmail.com>
2023-08-17 08:04:52 -0400

    Implementated authentication for signin with post handler on /signin route.

--- controllers/users.go
+++ controllers/users.go
@@ -45,3 +45,20 @@ func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
 	data.Email = r.FormValue("email")
 	u.Templates.SignIn.Execute(w, data)
 }
+
+func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something Went Wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User Authenticated: %+v", user)
+}
--- main.go
+++ main.go
@@ -17,25 +17,6 @@ func main() {
 	log := controllers.NewLogger()
 
 	r := chi.NewRouter()
-
-	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(
-			templates.FS,
-			"home.html", "tailwind.html",
-		))))
-
-	r.Get("/about", controllers.About(
-		views.Must(views.ParseFS(
-			templates.FS,
-			"about.html", "tailwind.html",
-		))))
-
-	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(
-			templates.FS,
-			"contact.html", "tailwind.html",
-		))))
-
 	cfg := models.DefaultPostgresConfig()
 	db, err := models.Open(cfg)
 	if err != nil {
@@ -43,26 +24,47 @@ func main() {
 	}
 	defer db.Close()
 
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(
+			templates.FS,
+			"home.html", "tailwind.html",
+		))))
+	r.Get("/about", controllers.About(
+		views.Must(views.ParseFS(
+			templates.FS,
+			"about.html", "tailwind.html",
+		))))
+	r.Get("/contact", controllers.StaticHandler(
+		views.Must(views.ParseFS(
+			templates.FS,
+			"contact.html", "tailwind.html",
+		))))
+
 	userService := models.UserService{
 		DB: db,
 	}
 	usersC := controllers.Users{
 		UserService: &userService,
 	}
+
 	usersC.Templates.New = views.Must(views.ParseFS(
 		templates.FS,
 		"signup.html", "tailwind.html",
 	))
+	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
+
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
 		templates.FS,
 		"signin.html", "tailwind.html",
 	))
-	r.Get("/signup", usersC.New)
 	r.Get("/signin", usersC.SignIn)
-	r.Post("/users", usersC.Create)
+	r.Post("/signin", usersC.ProcessSignIn)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
+
 	fmt.Println("Starting server on :3000...")
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Debug("http.ListenAndServe failed", slog.Any("err", err))
