52d8f6a41bfde1c9d1e099e33b507c9e2d30dd0f
simon-lentz <simonlentz1@gmail.com>
2023-08-17 13:49:36 -0400

    Optimized some of the SQL.

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -1,5 +1,10 @@
-CREATE TABLE sessions (
+CREATE TABLE IF NOT EXISTS sessions (
     id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
+    user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
     token_hash TEXT UNIQUE NOT NULL
 );
+
+ --ALTER TABLE sessions
+ --   ADD CONSTRAINT sessions_user_id_fkey
+ --   FOREIGN KEY (user_id)
+ --   REFERENCES users (id);
