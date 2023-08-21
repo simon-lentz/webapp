6712098352e5435370af5d28758772c68c786cdc
simon-lentz <simonlentz1@gmail.com>
2023-08-21 09:47:10 -0400

    removed some duplication from galleries.go controller with galleryByID helper func

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -37,39 +37,25 @@ func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
 	}
 	data.UserID = context.User(r.Context()).ID
 	data.Title = r.FormValue("title")
-
 	gallery, err := g.GalleryService.Create(data.Title, data.UserID)
 	if err != nil {
 		g.Templates.New.Execute(w, r, data, err)
 		return
 	}
-
 	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
 	http.Redirect(w, r, editPath, http.StatusFound)
 }
 
 func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	gallery, err := g.galleryByID(w, r)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusNotFound)
 		return
 	}
-	gallery, err := g.GalleryService.ByID(uint(id))
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.Error(w, "Gallery Not Found", http.StatusNotFound)
-			return
-		}
-		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
-		return
-	}
-
 	user := context.User(r.Context())
 	if gallery.UserID != user.ID {
 		http.Error(w, "Gallery Not Found", http.StatusNotFound)
 		return
 	}
-
 	var data struct {
 		ID    uint
 		Title string
@@ -80,33 +66,20 @@ func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 }
 
 func (g Galleries) Update(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	gallery, err := g.galleryByID(w, r)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusNotFound)
 		return
 	}
-	gallery, err := g.GalleryService.ByID(uint(id))
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.Error(w, "Gallery Not Found", http.StatusNotFound)
-			return
-		}
-		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
-		return
-	}
-
 	user := context.User(r.Context())
 	if gallery.UserID != user.ID {
 		http.Error(w, "Gallery Not Found", http.StatusNotFound)
 		return
 	}
-
 	gallery.Title = r.FormValue("title")
 	if err = g.GalleryService.Update(gallery); err != nil {
 		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
 		return
 	}
-
 	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
 	http.Redirect(w, r, editPath, http.StatusFound)
 }
@@ -117,44 +90,29 @@ func (g Galleries) Index(w http.ResponseWriter, r *http.Request) {
 		ID    uint
 		Title string
 	}
-
 	var data struct {
 		Galleries []Gallery
 	}
-
 	user := context.User(r.Context())
 	galleries, err := g.GalleryService.ByUserID(user.ID)
 	if err != nil {
 		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
 		return
 	}
-
 	for _, gallery := range galleries {
 		data.Galleries = append(data.Galleries, Gallery{
 			ID:    gallery.ID,
 			Title: gallery.Title,
 		})
 	}
-
 	g.Templates.Index.Execute(w, r, data)
 }
 
 func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	gallery, err := g.galleryByID(w, r)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusNotFound)
 		return
 	}
-
-	gallery, err := g.GalleryService.ByID(uint(id))
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.Error(w, "Gallery Not Found", http.StatusNotFound)
-			return
-		}
-		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
-	}
-
 	var data struct {
 		ID     uint
 		Title  string
@@ -168,6 +126,23 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
 		catImageURL := fmt.Sprintf("https://placekitten.com/%d/%d", width, height)
 		data.Images = append(data.Images, catImageURL)
 	}
-
 	g.Templates.Show.Execute(w, r, data)
+}
+
+func (g Galleries) galleryByID(w http.ResponseWriter, r *http.Request) (*models.Gallery, error) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusNotFound)
+		return nil, err
+	}
+	gallery, err := g.GalleryService.ByID(uint(id))
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			http.Error(w, "Gallery Not Found", http.StatusNotFound)
+			return nil, err
+		}
+		http.Error(w, "Something Went Wrong", http.StatusInternalServerError)
+		return nil, err
+	}
+	return gallery, nil
 }
